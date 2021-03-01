357bccf56cf2b714279dba82ab1247556fee40fc
nikhilkumbhare <nikhilk190697@gmail.com>
2021-03-01 22:30:07 +0530

    Added: To Enter a Valid Last Name

--- UserRegistration.java
+++ UserRegistration.java
@@ -18,10 +18,25 @@ public class UserRegistration{
 		}
 	}
 
+	static void userLastName() {
+
+		String lastName = user.nextLine();
+		boolean result = Pattern.matches("^[A-Z]{1}[a-z]{2,14}$",lastName);
+		if (result == true) {
+			System.out.println("You Have Enter Valid Last Name: " +lastName);
+		}else {
+			System.out.println("Not a Valid Name");
+			System.out.println("Please Re-enter The Last Name: ");
+			userLastName();
+		}
+	}
+
 	public static void main(String[] args){
 
 	System.out.println(" Enter First Name ");
 	userFirstName();
+	System.out.println(" Enter Last Name ");
+	userLastName();
 
    }
 }
